b0622ef3edadc4038aafa3f38cf132196c1f8cb7
MVeneroY <veneromiguel337@gmail.com>
2023-02-23 14:36:56 -0500

    .

--- Runner.java
+++
@@ -1,8 +0,0 @@
-class Runner {
-    public static void main(String[] args) {
-        int n = 0;
-        if (args.length == 1) n = Integer.parseInt(args[0]);
-
-        System.out.println(n + " threads started");
-    }
-}
